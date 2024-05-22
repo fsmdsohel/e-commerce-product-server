dd425ce810d5c9bbfc13951d398ccef0bc06f138
fsmdsohel <sohel.rana.coder@gmail.com>
2024-05-22 21:56:15 +0600

    feat: Update app.ts to handle 404 errors
    This commit modifies the `app.ts` file to handle 404 errors. It includes the following changes:
    
    - Added a new middleware function to handle requests that do not match any defined routes.
    - The middleware function sends a JSON response with a 404 status code and a message indicating that the route was not found.
    
    These changes ensure that when a user requests a route that does not exist, the server responds with a proper 404 error message.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { ProductRoutes } from "./app/modules/product/product.route";
 import { OrderRoutes } from "./app/modules/order/order.route";
 
@@ -14,10 +14,16 @@ app.use("/api/products", ProductRoutes);
 app.use("/api/orders", OrderRoutes);
 
 // default route
-app.get("/", async (req, res) => {
+app.get("/", async (req: Request, res: Response) => {
   res
     .status(200)
     .json({ success: true, message: "Welcome to the E-commerce API" });
+});
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
 });
 
 export default app;
--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,10 +1,33 @@
+import Product from "../product/product.model";
 import { TOrder } from "./order.interface";
 import Order from "./order.model";
 
 const createNewOrderIntoDB = async (orderData: TOrder) => {
   try {
+    const product = await Product.findById(orderData.productId);
+
+    if (!product) {
+      throw new Error("Order not found");
+    }
+
+    if (orderData.quantity > product.inventory.quantity) {
+      throw new Error("Insufficient quantity available in inventory");
+    }
+
+    const newQuantity = product.inventory.quantity - orderData.quantity;
+
+    await Product.updateOne(
+      { _id: orderData.productId },
+      {
+        $set: {
+          "inventory.quantity": newQuantity,
+          "inventory.inStock": newQuantity <= 0 ? false : true,
+        },
+      }
+    );
     const newOrder = new Order(orderData);
     const result = await newOrder.save();
+
     return result;
   } catch (error: any) {
     throw new Error(error);
